13a7fb75f444d0919938ac6c08ca46a6be039d6b
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-12-21 02:50:08 +0530

    WIP Issue_comments and Pull_requests pending

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.get("/test", async (c) => {
   }, 200);
 });
 
-app.post('/github/:webhook', async (c) => {
+app.post('/:webhook', async (c) => {
   try {
     const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
     const client = new Client(DATABASE_URL);
@@ -37,17 +37,144 @@ app.post('/github/:webhook', async (c) => {
           return c.json({}, 200);
         } catch (error) {
           console.log(error);
-          return c.json({}, 400);
+          return c.json({}, 500);
         }
+      // For handling of issue labelling, unlabelling, assigning, unassigning
       case 'issues':
-        // For handling issue comments of the type : 
-        // 1. /bounty 50 @username
-        // 2. /remove 50 @username
-        try {
+        const action = payload.action;
+        switch (action) {
+          // Insertion for labelling an issue as AMWOC
+          case "labeled":
+            const wocLabelAdded = payload.label.name === "AMWOC";
+            if (wocLabelAdded !== true) {
+              return c.json({}, 200);
+            }
+            try {
+              const query = `INSERT INTO "Issue" ("issueId", "repoId", "url") VALUES($1, $2, $3)`;
+              const values = [payload.issue.id, payload.issue.url, payload.repository.id];
+              await client.query(query, values);
+              return c.json({}, 200);
+            } catch (error) {
+              return c.json({}, 500);
+            }
 
-          return c.status(200); // Success
-        } catch (error) {
+          // Deletion for unlabelling an issue which was AMWOC
+          case "unlabeled":
+            const wocLabelRemoved = payload.label.name === "AMWOC";
+            if (wocLabelRemoved !== true) {
+              return c.json({}, 200);
+            }
+            try {
+              const query = `DELETE FROM "Issue" WHERE "issueId" = $1`;
+              const values = [payload.issue.id];
+              await client.query(query, values);
+              return c.json({}, 200);
+            } catch (error) {
+              return c.json({
+                message: "Internal Server Error",
+              }, 500);
+            }
 
+          // Updation of issue for assignment
+          case "assigned":
+            try {
+              await client.query("BEGIN");
+
+              const checkQuery = `SELECT EXISTS(SELECT 1 from "Issue" WHERE "issueId" = $1`;
+              const checkResult = await client.query(checkQuery, [payload.issue.id]);
+
+              if (!checkResult.rows[0].exists) {
+                await client.query("COMMIT");
+                return c.json({}, 200);
+              }
+
+              const updateQuery = `UPDATE "Issue" SET "claimedBy" = $1 WHERE "issueId" = $2`;
+              const updateValues = [payload.issue.assignee, payload.issue.id];
+              await client.query(updateQuery, updateValues);
+
+              await client.query("COMMIT");
+
+              return c.json({}, 200);
+            } catch (error) {
+              await client.query("ROLLBACK");
+              console.log(error);
+              return c.json({
+                message: "Internal Server Error",
+              }, 500);
+            }
+          // Updation of issue for unassignment
+          case "unassigned":
+            try {
+              await client.query("BEGIN");
+
+              const checkQuery = `SELECT EXISTS(SELECT 1 from "Issue" WHERE "issueId" = $1`;
+              const checkResult = await client.query(checkQuery, [payload.issue.id]);
+
+              if (!checkResult.rows[0].exists) {
+                await client.query("COMMIT");
+                return c.json({}, 200);
+              }
+
+              const updateQuery = `UPDATE "Issue" SET "claimedBy" = NULL WHERE "issueId" = $1`;
+              await client.query(updateQuery, [payload.issue.id]);
+              await client.query("COMMIT");
+
+              return c.json({}, 200);
+            } catch (error) {
+              await client.query("ROLLBACK");
+              console.log(error);
+              return c.json({
+                message: "Internal Server Error",
+              }, 500);
+            }
+          case "closed":
+            try {
+              await client.query("BEGIN");
+
+              const checkQuery = `SELECT EXISTS(SELECT 1 from "Issue" WHERE "issueId" = $1`;
+              const checkResult = await client.query(checkQuery, [payload.issue.id]);
+
+              if (!checkResult.rows[0].exists) {
+                await client.query("COMMIT");
+                return c.json({}, 200);
+              }
+
+              const updateQuery = `UPDATE "Issue" SET "issueStatus" = false WHERE "issueId" = $1`;
+              await client.query(updateQuery, [payload.issue.id]);
+              await client.query("COMMIT");
+              return c.json({}, 200);
+            } catch (error) {
+              await client.query("ROLLBACK");
+              console.log(error);
+              return c.json({
+                message: "Internal Server Error",
+              }, 500);
+            }
+          case "reopened":
+            try {
+              await client.query("BEGIN");
+
+              const checkQuery = `SELECT EXISTS(SELECT 1 from "Issue" WHERE "issueId" = $1`;
+              const checkResult = await client.query(checkQuery, [payload.issue.id]);
+
+              if (!checkResult.rows[0].exists) {
+                await client.query("COMMIT");
+                return c.json({}, 200);
+              }
+
+              const updateQuery = `UPDATE "Issue" SET "issueStatus" = false WHERE "issueId" = $1`;
+              await client.query(updateQuery, [payload.issue.id]);
+              await client.query("COMMIT");
+              return c.json({}, 200);
+            } catch (error) {
+              await client.query("ROLLBACK");
+              console.log(error);
+              return c.json({
+                message: "Internal Server Error",
+              }, 500);
+            }
+          default:
+            break;
         }
       case 'issue_comment':
         try {
