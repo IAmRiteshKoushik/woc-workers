725a72e1fa3000bccbd98c6d59cf0fc22dde990a
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-12-20 21:16:21 +0530

    Test endpoint added for cf

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { Client } from "@neondatabase/serverless";
+import { env } from "hono/adapter";
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get("/test", async (c) => {
+  console.log("Testing endpoint");
+  return c.json({
+    message: "Hello from cloudflare"
+  }, 200);
 })
 
+app.post('/github/:id', async (c) => {
+  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
+  const client = new Client(DATABASE_URL);
+  await client.connect();
+  const { rows } = await client.query('SELECT * FROM books_to_read;');
+  console.log(rows);
+  return c.json({
+    message: "Webhook delivery complete",
+  }, 200);
+});
+
+/*
+ * WebHook for the following things:
+ * 1. Issue comments
+ * 2. Labels (Label created, edited or deleted)
+ * 3. Pull Request Reviews
+ */
+
 export default app
+
+/* -- Bounty Manager
+ * "/bounty 25 @IAmRiteshKoushik"
+ */
